ac9b0ef983385ecc063a4e377cb3151c6861223f
sou - rufus <soubougouin@gmail.com>
2022-10-15 16:22:50 +0200

    système d'ajout des membres en HTML + suppression

--- web-page/assets/js/member.js
+++ web-page/assets/js/member.js
@@ -37,10 +37,49 @@ class Member {
                 "gender": null,
                 "nationality": null
             });
+            let createHTMLmemberReturn = await this.createHTMLmember(this.family.find(e => e.id == memberID))
+            if (createHTMLmemberReturn.completed) {
+                resolve({
+                    completed: true,
+                    family: this.family
+                });
+            } else {
+                resolve({
+                    completed: false,
+                    error: createHTMLmemberReturn.error
+                })
+            }
+        });
+    }
+    createHTMLmember(member) {
+        return new Promise(async resolve => {
+            try {
+                let row = document.querySelector('.row') ? document.querySelectorAll('.row')[this.family.find(m => m.children.indexOf(member.id) != -1) == undefined ? 0 : 1] : undefined
+                if (!row) {
+                    let rowDiv = document.createElement('div');
+                    rowDiv.classList.add('row')
+                    document.querySelector('.container').appendChild(rowDiv);
+                }
+                row = document.querySelector('.row') ? document.querySelectorAll('.row')[this.family.find(m => m.children.indexOf(member.id) != -1) == undefined ? 0 : 1] : undefined
+                row.innerHTML += `
+                    <div class="member hide" id="${member.id}" onclick="memberClicked(this);">
+                        <div class="picture">
+                            <img src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png?w=360" alt="Picture">
+                        </div>
+                        <p class="name"><span class="hider-1"></span><span class="editable-name" contenteditable="true">Prénom</span><span class="hider-2"></span></p>
+                    </div>
+                `;
+                setTimeout(() => { row.querySelector('.member.hide').classList.remove('hide') }, 10);
+            } catch (err) {
+                resolve({
+                    completed: false,
+                    error: err
+                });
+            }
             resolve({
                 completed: true,
                 family: this.family
-            });
+            })
         });
     }
     removeMember(id) {
@@ -48,10 +87,10 @@ class Member {
             if (id.match(/([A-Z0-9]{6})/g)) {
                 if (this.family.find(member => member.id == id)) {
                     this.family = this.family.filter(member => member.id != id);
-                    return {
+                    resolve({
                         completed: true,
                         family: this.family
-                    }
+                    })
                 } else {
                     resolve({
                         completed: false,
@@ -66,7 +105,32 @@ class Member {
             }
         })
     }
+    getMember(id) {
+        return new Promise(async resolve => {
+            if (id.match(/([A-Z0-9]{6})/g)) {
+                if (this.family.find(member => member.id == id)) {
+                    resolve({
+                        completed: true,
+                        member: this.family.find(member => member.id == id),
+                        family: this.family
+                    });
+                } else {
+                    resolve({
+                        completed: false,
+                        error: `[${id}] : Membre non trouvé`
+                    })
+                }
+            } else {
+                resolve({
+                    completed: false,
+                    error: `[${id}] : Ceci n'est pas un ID valide`
+                })
+            }
+        });
+    }
     editMember(memberEdited) {
 
     }
+
+
 }
